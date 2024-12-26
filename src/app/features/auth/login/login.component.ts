7f7ac7c342c215dc8d369555fa5ca2d50780cc44
0marAmara <onan383@outlook.com>
2024-12-26 19:22:51 +0200

    Implemented the reactive and template driven signup and login forms

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {FormFieldComponent} from '../../../shared/form-field/form-field.component';
+import {FormsModule, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
+    RouterLink,
+    FormFieldComponent,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  protected readonly onsubmit = onsubmit;
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -1,5 +1,13 @@
-import {Component, ViewChild} from '@angular/core';
-import {FormsModule, NgForm} from '@angular/forms';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import {PasswordMatchDirective} from './password-match.directive';
 import {RouterLink} from '@angular/router';
 import {FormFieldComponent} from '../../../shared/form-field/form-field.component';
@@ -8,17 +16,43 @@ import {FormFieldComponent} from '../../../shared/form-field/form-field.componen
   selector: 'app-signup',
   imports: [
     FormsModule,
-    PasswordMatchDirective,
     RouterLink,
-    FormFieldComponent
+    FormFieldComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
-  @ViewChild('signupForm') signupForm!: NgForm;
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
-  onSubmit() {
-    console.log(this.signupForm);
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+        'firstName': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'lastName': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'username': new FormControl('', [Validators.required, Validators.minLength(4)]),
+        'email': new FormControl('', [Validators.required, Validators.email]),
+        'password': new FormControl('', [Validators.required, Validators.minLength(6)]),
+        'confirmPassword': new FormControl('', [Validators.required, Validators.minLength(6)]),
+      }, {
+        validators: this.passwordMatchValidator
+      }
+    )
+  }
+
+  formInputError(name: string) {
+    return !this.signupForm.get(name)!.valid && this.signupForm.get(name)!.touched;
+  }
+
+  passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const group = control as FormGroup;
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : {mismatch: true};
+  }
+
+  onSubmit(): void {
+    console.log('Form submitted:', this.signupForm.value);
+
   }
 }
