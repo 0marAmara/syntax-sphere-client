4000d6234c6bbbf3e8140875fc6e8da09e519f02
0marAmara <onan383@outlook.com>
2025-01-04 23:36:25 +0200

    added the new appInputField component to the login component

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,30 +1,46 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
 import {FormFieldComponent} from '../../../shared/form-field/form-field.component';
-import {FormGroup, FormsModule, NgForm} from '@angular/forms';
+import {FormControl, FormGroup, FormsModule, NgForm, Validators} from '@angular/forms';
 import {ButtonComponent} from '../../../shared/button/button.component';
 import {AuthService} from '../../../core/services/auth.service';
 import {LoginUser} from '../../../shared/models/user.model';
+import {InputFieldComponent} from '../../../shared/form/input-field/input-field.component';
 
 @Component({
   selector: 'app-login',
   imports: [
     RouterLink,
-    FormFieldComponent,
     FormsModule,
-    ButtonComponent
+    ButtonComponent,
+    InputFieldComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private authService = inject(AuthService);
   private router = inject(Router);
-  signupForm!: FormGroup;
-  errorMessage: string="";
+  loginForm!: FormGroup;
+  errorMessage: string = "";
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      'username': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required]),
+    })
+  }
+
+  get usernameControl(): FormControl {
+    return this.loginForm.get('username') as FormControl;
+  }
+
+  get passwordControl(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
 
   onSubmit(form: NgForm) {
-    this.errorMessage="";
+    this.errorMessage = "";
     const user: LoginUser = {
       username: form.value.username,
       password: form.value.password,
