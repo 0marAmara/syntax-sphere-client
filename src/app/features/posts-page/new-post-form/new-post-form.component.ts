2924a55605a1790ddda643de246dfb754d411984
0marAmara <onan383@outlook.com>
2025-02-01 00:51:49 +0200

    Implemented the optional media for posts

--- src/app/features/posts-page/new-post-form/new-post-form.component.ts
+++ src/app/features/posts-page/new-post-form/new-post-form.component.ts
@@ -1,10 +1,10 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {TextAreaFieldComponent} from '../../../shared/form/text-area-field/text-area-field.component';
+import {TextAreaFieldComponent} from '@shared/form/text-area-field/text-area-field.component';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {ButtonComponent} from '../../../shared/button/button.component';
-import {InputFieldComponent} from '../../../shared/form/input-field/input-field.component';
-import {PostsService} from '../../../core/services/posts.service';
-import {PostElement} from '../../../shared/models/post.model';
+import {ButtonComponent} from '@shared/button/button.component';
+import {InputFieldComponent} from '@shared/form/input-field/input-field.component';
+import {PostsService} from '@core/services';
+import {FileUploadComponent} from '@shared/form/file-upload/file-upload.component';
 
 @Component({
   selector: 'app-new-post-form',
@@ -17,6 +17,7 @@ import {PostElement} from '../../../shared/models/post.model';
     TextAreaFieldComponent,
     TextAreaFieldComponent,
     TextAreaFieldComponent,
+    FileUploadComponent,
   ],
   templateUrl: './new-post-form.component.html',
   styleUrl: './new-post-form.component.scss'
@@ -27,14 +28,19 @@ export class NewPostFormComponent implements OnInit {
   postsService = inject(PostsService);
   form!: FormGroup;
   urlIsEnabled = false;
+  image?:File;
 
   ngOnInit() {
     this.form = new FormGroup({
       title: new FormControl('', [Validators.required, Validators.minLength(4)]),
       content: new FormControl('', [Validators.required, Validators.minLength(10)]),
-      //TODO update the link validator
-      url: new FormControl('', [Validators.pattern('https?:\/\/(?:www\.)?[^\s\/$.?#].[^\s]*'),]),
+      image: new FormControl('', []),
+      url: new FormControl('', [Validators.pattern('^(https?:\/\/)?([^\s\/$.?#]+\.)+[^\s\/$.?#]+[^\s]*$')]),
     })
+  }
+
+  setImage(image: File) {
+    this.image = image;
   }
 
   get titleControl(): FormControl {
@@ -45,19 +51,29 @@ export class NewPostFormComponent implements OnInit {
     return this.form.get('content') as FormControl;
   }
 
+  get imageControl(): FormControl {
+    return this.form.get('image') as FormControl;
+  }
+
   get urlControl(): FormControl {
     return this.form.get('url') as FormControl;
   }
 
   onSubmit() {
-    const newPost: PostElement = {
-      title: this.form.value.title,
-      content: this.form.value.content,
-    }
+    const formData = new FormData();
+
+    formData.append('title', this.form.value.title);
+    formData.append('content', this.form.value.content);
+
     if (this.form.value.url) {
-      newPost.url = this.form.value.url;
+      formData.append('url', this.form.value.url);
     }
-    this.postsService.addPost(newPost).subscribe(() => {
+
+    if (this.image) {
+      formData.append('image', this.image);
+    }
+
+    this.postsService.addPost(formData).subscribe(() => {
       this.postsService.loadPosts();
       this.form.reset();
       this.urlIsEnabled = false;
